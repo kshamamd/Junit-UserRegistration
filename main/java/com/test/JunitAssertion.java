0156dab3fd256eb9eac5f6266eceb06c771c2fbd
kshama <kshamamd@gmail.com>
2021-04-05 00:22:43 +0530

    [kshama]:validated by Junit testig-moodanalyser

--- main/java/com/test/JunitAssertion.java
+++ main/java/com/test/JunitAssertion.java
@@ -3,7 +3,43 @@ package com.test;
 import java.util.regex.Pattern;
 
 public class JunitAssertion {
-    public Boolean firstName(String a) {
+    public String firstName(String a) {
+        Boolean firstNameValidation = Pattern.matches("^[A-Z]{1}[a-z]{2,}$",a);
+        if(firstNameValidation == true)
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+    public String lastName(String a) {
+        Boolean lastNameValidation = Pattern.matches("^[A-Z]{1}[a-z]{2,}$",a);
+        if(lastNameValidation == true)
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+    public String email(String a) {
+        Boolean emailValidation = Pattern.matches("^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.\\-]+\\.[A-Za-z]{2,6}",a);
+        if(emailValidation == true)
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+    public String number(String a) {
+        Boolean numberValidation = Pattern.matches("^[91]+[\\s]+[0-9]{10}",a);
+        if(numberValidation == true)
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+    public String password(String a) {
+        Boolean passwordValidation = Pattern.matches("[A-Za-z0-9]*[^A-Za-z0-9]{1}[A-Za-z0-9]*",a);
+        if(passwordValidation == true)
+            return "HAPPY";
+        else
+            return "SAD";
+    }
+
+    /*public Boolean firstName(String a) {
         Boolean firstNameValidation = Pattern.matches("^[A-Z]{1}[a-z]{2,}$",a);
         return firstNameValidation;
     }
@@ -19,4 +55,5 @@ public class JunitAssertion {
         return emailValidation;
 
     }
+*/
 }
--- test/java/com/validator/Validate.java
+++ test/java/com/validator/Validate.java
@@ -9,31 +9,42 @@ import java.sql.SQLOutput;
 public class Validate {
     @Test
     void Check_Firstname_WhileCorrect_Return_True() {
-
         JunitAssertion fName = new JunitAssertion();
-        Boolean ch = fName.firstName("Kshama");
-        Assertions.assertTrue(ch);
+        String ch = fName.firstName("Kshama");
+        Assertions.assertEquals("HAPPY", ch);
+        System.out.println(ch);
     }
 
     @Test
     void Check_Lastname_WhileCorrect_Return_True() {
+
         JunitAssertion lName = new JunitAssertion();
-        Boolean ch = lName.lastName("Deshmukh");
-        Assertions.assertEquals(true, ch);
+        String ch = lName.lastName("Deshmukh");
+        Assertions.assertEquals("HAPPY", ch);
+        System.out.println(ch);
+    }
+
+    @Test
+    void Check_Email_WhileCorrect_Return_True() {
+
+        JunitAssertion email = new JunitAssertion();
+        String ch = email.email("kshama@gmail.com");
+        Assertions.assertEquals("HAPPY", ch);
         System.out.println(ch);
     }
     @Test
-    void Check_Email_WhileCorrect_Return_True() {
-        JunitAssertion email = new JunitAssertion();
-        Boolean ch = email.email("shama@gmail.com");
-        Assertions.assertTrue(ch);
-        System.out.println(true);
+    void Check_Number_WhileCorrect_Return_True() {
+
+        JunitAssertion number = new JunitAssertion();
+        String ch = number.number("91 1234567890");
+        Assertions.assertEquals("HAPPY", ch);
+        System.out.println(ch);
     }
     @Test
-    public void givenEmailIsValidShouldReturnTrue() {
-        JunitAssertion email = new JunitAssertion();
-        boolean result = email.email("abc-100@gmail.com.au"); //accepts all the sample emails
-        Assertions.assertTrue(result);
+    void Check_Password_WhileCorrect_Return_True() {
+        JunitAssertion password = new JunitAssertion();
+        String ch = password.password("Kshama@123");
+        Assertions.assertEquals("HAPPY", ch);
+        System.out.println(ch);
     }
-
 }
